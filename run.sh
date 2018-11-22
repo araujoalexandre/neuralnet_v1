82052cd6fe09797cbd47bb4b33d25d25a801bb9c
Alexandre Araujo <aaraujo001@gmail.com>
2018-11-22 23:36:24 +0100

    add run script

---
+++ run.sh
@@ -0,0 +1,21 @@
+
+MODELS_PATH="/home/aaraujo/neuralnet_structured_matrices/models"
+TRAIN_DIR=$(date '+%Y-%m-%d_%H.%M.%S')
+
+DIR_LOGS="${MODELS_PATH}/${TRAIN_DIR}_logs"
+mkdir ${DIR_LOGS}
+cp ./config.yaml ${DIR_LOGS}/model_flags.yaml
+
+nohup python3 code/train.py \
+	--config_file=config.yaml \
+	--config_name=train \
+	--train_dir=${TRAIN_DIR} \
+  &>> "${DIR_LOGS}/log_train.logs" &
+
+
+nohup python3 code/eval.py \
+	--config_file=config.yaml \
+	--config_name=eval \
+  --train_dir=${TRAIN_DIR} \
+  &>> "${DIR_LOGS}/log_eval.logs" &
+
