89cc96b5448800c1471a0204d1a9fa79a9b21759
Alexandre Araujo <aaraujo001@gmail.com>
2018-12-21 20:38:57 +0100

    eval under attack api update

--- code/attacks.py
+++ code/attacks.py
@@ -1,9 +1,20 @@
-
+import numpy as np
 import tensorflow as tf
 from cleverhans import attacks as cleverhans_attack
 
+from cleverhans import utils
+from cleverhans.attacks_tf import SPSAAdam, margin_logit_loss, TensorAdam
+from cleverhans.model import Model, CallableModelWrapper
+from cleverhans.model import wrapper_warning, wrapper_warning_logits
+from cleverhans.compat import reduce_sum, reduce_mean
+from cleverhans.compat import reduce_max
+from cleverhans.compat import softmax_cross_entropy_with_logits
+from cleverhans.utils_tf import clip_eta
+from cleverhans import utils_tf
+
 from config import YParams
 from config import hparams as FLAGS
+
 
 class Attacks:
 
@@ -12,34 +23,180 @@ class Attacks:
     self.sess = sess
     attack_cls = getattr(cleverhans_attack, FLAGS.attack_method)
     self.attack = attack_cls(model, sess=sess)
-    self.generate = getattr(self, FLAGS.attack_method)
 
-  def CarliniWagnerL2(self, images):
-    params = self.config['CarliniWagnerL2']
-    cw_params = {
-          'binary_search_steps': params['binary_search_steps'],
-          'max_iterations': params['max_iterations'],
-          'learning_rate': params['learning_rate'],
-          'batch_size': images.shape[0],
-          'initial_const': params['initial_const']
-    }
-    images_adv = self.attack.generate_np(images, **cw_params)
+    # get attack params
+    self.kwargs = self.config.get(FLAGS.attack_method, {})
+    for k, v in self.kwargs.items():
+      if v == "default":
+        del self.kwargs[k]
+
+  def generate(self, images, labels):
+    if getattr(self.attack, "generate"):
+      tf_imgs = tf.convert_to_tensor(images)
+      tf_labels = tf.convert_to_tensor(labels)
+      images_adv = self.sess.run(
+        self.attack.generate(tf_imgs, **self.kwargs))
+    elif getattr(self.attack, "generate_np"):
+      images_adv = self.attack.generate_np(images, **self.kwargs)
+
     return images_adv
 
-  def FastGradientMethod(self, images):
-    params = self.config['FastGradientMethod']
-    fgm_params = {
-      'eps': params['eps'],
-      'clip_min': params['clip_min'],
-      'clip_max': params['clip_max'],
-    }
-    tf_imgs = tf.convert_to_tensor(images)
-    images_adv = self.sess.run(self.attack.generate(tf_imgs, **fgm_params))
-    return images_adv
+class FastGradientMethod:
+  """
+  This attack was originally implemented by Goodfellow et al. (2015) with the
+  infinity norm (and is known as the "Fast Gradient Sign Method"). This
+  implementation extends the attack to other norms, and is therefore called
+  the Fast Gradient Method.
+  Paper link: https://arxiv.org/abs/1412.6572
+  :param model: cleverhans.model.Model
+  :param sess: optional tf.Session
+  :param dtypestr: dtype of the data
+  :param kwargs: passed through to super constructor
+  """
+  def __init__(self, eps=0.3, ord=np.inf, clip_min=None, clip_max=None):
+    """
+    Create a FastGradientMethod instance.
+    Note: the model parameter should be an instance of the
+    cleverhans.model.Model abstraction provided by CleverHans.
+    """
+    self.eps = eps
+    self.ord = ord
+    self.clip_min = clip_min
+    self.clip_max = clip_max
 
-  def generate_adv(self, images):
-    images_adv = self.generate(images)
-    if self.config['adv_processing']:
-      images_adv *= np.float32(np.int64(images_adv * 255))
-      images_adv /= 255
-    return images_adv
+    if self.ord == 'inf':
+      self.ord = np.inf
+
+  def generate(self, x, logits):
+    """
+    Returns the graph for Fast Gradient Method adversarial examples.
+    :param x: The model's symbolic inputs.
+    :param kwargs: See `parse_params`
+    """
+    return fgm(
+        x,
+        logits,
+        y=None,
+        eps=self.eps,
+        ord=self.ord,
+        clip_min=self.clip_min,
+        clip_max=self.clip_max,
+        targeted=None,
+        sanity_checks=False)
+
+
+
+def fgm(x,
+        logits,
+        y=None,
+        eps=0.3,
+        ord=np.inf,
+        clip_min=None,
+        clip_max=None,
+        targeted=False,
+        sanity_checks=True):
+  """
+  TensorFlow implementation of the Fast Gradient Method.
+  :param x: the input placeholder
+  :param logits: output of model.get_logits
+  :param y: (optional) A placeholder for the true labels. If targeted
+            is true, then provide the target label. Otherwise, only provide
+            this parameter if you'd like to use true labels when crafting
+            adversarial samples. Otherwise, model predictions are used as
+            labels to avoid the "label leaking" effect (explained in this
+            paper: https://arxiv.org/abs/1611.01236). Default is None.
+            Labels should be one-hot-encoded.
+  :param eps: the epsilon (input variation parameter)
+  :param ord: (optional) Order of the norm (mimics NumPy).
+              Possible values: np.inf, 1 or 2.
+  :param clip_min: Minimum float value for adversarial example components
+  :param clip_max: Maximum float value for adversarial example components
+  :param targeted: Is the attack targeted or untargeted? Untargeted, the
+                   default, will try to make the label incorrect. Targeted
+                   will instead try to move in the direction of being more
+                   like y.
+  :return: a tensor for the adversarial example
+  """
+
+  # Make sure the caller has not passed probs by accident
+  assert logits.op.type != 'Softmax'
+
+  # Using model predictions as ground truth to avoid label leaking
+  preds_max = reduce_max(logits, 1, keepdims=True)
+  y = tf.to_float(tf.equal(logits, preds_max))
+  y = tf.stop_gradient(y)
+  y = y / reduce_sum(y, 1, keepdims=True)
+
+  # Compute loss
+  loss = softmax_cross_entropy_with_logits(labels=y, logits=logits)
+  if targeted:
+    loss = -loss
+
+  # Define gradient of loss wrt input
+  grad, = tf.gradients(loss, x)
+
+  optimal_perturbation = optimize_linear(grad, eps, ord)
+
+  # Add perturbation to original example to obtain adversarial example
+  adv_x = x + optimal_perturbation
+
+  # If clipping is needed, reset all values outside of [clip_min, clip_max]
+  if (clip_min is not None) or (clip_max is not None):
+    # We don't currently support one-sided clipping
+    assert clip_min is not None and clip_max is not None
+    adv_x = utils_tf.clip_by_value(adv_x, clip_min, clip_max)
+
+  if sanity_checks:
+    with tf.control_dependencies(asserts):
+      adv_x = tf.identity(adv_x)
+
+  return adv_x
+
+
+def optimize_linear(grad, eps, ord=np.inf):
+  """
+  Solves for the optimal input to a linear function under a norm constraint.
+  Optimal_perturbation = argmax_{eta, ||eta||_{ord} < eps} dot(eta, grad)
+  :param grad: tf tensor containing a batch of gradients
+  :param eps: float scalar specifying size of constraint region
+  :param ord: int specifying order of norm
+  :returns:
+    tf tensor containing optimal perturbation
+  """
+
+  red_ind = list(range(1, len(grad.get_shape())))
+  avoid_zero_div = 1e-12
+  if ord == np.inf:
+    # Take sign of gradient
+    optimal_perturbation = tf.sign(grad)
+    # The following line should not change the numerical results.
+    # It applies only because `optimal_perturbation` is the output of
+    # a `sign` op, which has zero derivative anyway.
+    # It should not be applied for the other norms, where the
+    # perturbation has a non-zero derivative.
+    optimal_perturbation = tf.stop_gradient(optimal_perturbation)
+  elif ord == 1:
+    abs_grad = tf.abs(grad)
+    sign = tf.sign(grad)
+    max_abs_grad = tf.reduce_max(abs_grad, red_ind, keepdims=True)
+    tied_for_max = tf.to_float(tf.equal(abs_grad, max_abs_grad))
+    num_ties = tf.reduce_sum(tied_for_max, red_ind, keepdims=True)
+    optimal_perturbation = sign * tied_for_max / num_ties
+  elif ord == 2:
+    square = tf.maximum(avoid_zero_div,
+                        reduce_sum(tf.square(grad),
+                                   reduction_indices=red_ind,
+                                   keepdims=True))
+    optimal_perturbation = grad / tf.sqrt(square)
+  else:
+    raise NotImplementedError("Only L-inf, L1 and L2 norms are "
+                              "currently implemented.")
+
+  # Scale perturbation to be the solution for the norm=eps rather than
+  # norm=1 problem
+  scaled_perturbation = utils_tf.mul(eps, optimal_perturbation)
+  return scaled_perturbation
+
+
+
+
