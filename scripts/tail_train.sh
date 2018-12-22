ae0337b54491341161766c8d52dc9f8502a8197e
Alexandre Araujo <aaraujo001@gmail.com>
2018-12-22 17:24:14 +0100

    update scripts

--- scripts/tail_train.sh
+++ scripts/tail_train.sh
@@ -1,2 +1,7 @@
 log_folder=$(find ${WORKDIR}/* -type d -iname "*logs" | sort | tail -n 1)
-tail -f ${log_folder}/log_train.logs
+if [ "$#" -ne 1 ]
+then
+  tail -f ${log_folder}/log_train.logs
+else
+  tail -f ${log_folder}/log_train.logs --lines=$1
+fi
