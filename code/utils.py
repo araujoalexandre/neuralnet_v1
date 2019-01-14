2f5960479980d47f4284d8e769a41f6cedbe3699
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-01-14 23:30:46 +0100

    add Dequantize function for youtube datasetÃ

--- code/utils.py
+++ code/utils.py
@@ -61,3 +61,20 @@ def l1_normalize(x, dim, epsilon=1e-12, name=None):
     abs_sum = tf.reduce_sum(tf.abs(x), dim, keep_dims = True)
     x_inv_norm = tf.reciprocal(tf.maximum(abs_sum, epsilon))
     return tf.multiply(x, x_inv_norm, name=name)
+
+def Dequantize(feat_vector, max_quantized_value=2, min_quantized_value=-2):
+  """Dequantize the feature from the byte format to the float format.
+
+  Args:
+    feat_vector: the input 1-d vector.
+    max_quantized_value: the maximum of the quantized value.
+    min_quantized_value: the minimum of the quantized value.
+
+  Returns:
+    A float vector which has the same shape as feat_vector.
+  """
+  assert max_quantized_value > min_quantized_value
+  quantized_range = max_quantized_value - min_quantized_value
+  scalar = quantized_range / 255.0
+  bias = (quantized_range / 512.0) + min_quantized_value
+  return feat_vector * scalar + bias
