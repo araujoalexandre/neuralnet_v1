436fabec712a6d2b6494997e156d15500a1739f0
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-01-24 00:05:36 +0100

    ACDC LayerÃ

--- code/layers.py
+++ code/layers.py
@@ -169,6 +169,103 @@ class CirculantLayer:
     return ret
 
 
+
+class ACDCLayer:
+
+  def __init__(self, shape_in, shape_out, start=False,
+               regularizer=None, use_bias=True,
+               sign_init=False, rand_init=False,
+               normal_init=False, alpha=1):
+
+    self.use_bias = use_bias
+    self.start = start
+    self.shape_in, self.shape_out = shape_in, shape_out
+    size = np.max([shape_in, shape_out])
+
+    self.size = np.max([shape_in, shape_out])
+    shape = (self.size, )
+
+    if normal_init:
+
+      kernel_initializer = tf.random_normal_initializer(
+        stddev=alpha/np.sqrt(shape_out))
+      self.kernel = tf.get_variable(name='kernel', shape=shape,
+        initializer=kernel_initializer, regularizer=regularizer)
+
+      diag_initializer = np.float32(np.random.choice([-1, 1], size=[shape_out]))
+      self.diag = tf.get_variable(name='diag', # shape=[shape_out],
+        initializer=diag_initializer, regularizer=regularizer)
+
+    else:
+
+
+      if sign_init:
+
+        diag_initializer = np.float32(np.random.choice([-1., 1.], size=[shape_in]))
+        self.diag = tf.get_variable(name='diag', # shape=[shape_in],
+                 initializer=diag_initializer, regularizer=regularizer)
+        # self.diag = self.diag / (3*28*28)
+        # self.A:uniform(-1, 1)
+        # self.A:copy(self.A:gt(0):float():add(-1, self.A:le(0):float()))
+
+        kernel_initializer = np.float32(np.random.choice([-1., 1.], size=[shape_in]))
+        self.kernel = tf.get_variable(name='kernel', # shape=[shape_in],
+                 initializer=kernel_initializer, regularizer=regularizer)
+        # self.kernel = self.kernel / (3*28*28)
+        # self.D:uniform(-1, 1)
+        # self.D:copy(self.D:gt(0):float():add(-1, self.D:le(0):float()))
+
+
+      else:
+        # self.A:fill(1)
+        # self.D:fill(1)
+        diag_initializer = tf.constant_initializer(1.)
+        self.diag = tf.get_variable(name='diag', shape=[shape_in],
+                 initializer=diag_initializer, regularizer=regularizer)
+
+        kernel_initializer = tf.constant_initializer(1.)
+        self.kernel = tf.get_variable(name='kernel', shape=[shape_in],
+                 initializer=kernel_initializer, regularizer=regularizer)
+
+      if rand_init:
+
+        # self.A:add(self.A:clone():uniform(-0.01, 0.01))
+        # self.D:add(self.D:clone():uniform(-0.01, 0.01))
+
+        noise = tf.random_uniform(tf.shape(self.diag), minval=-0.01, maxval=0.01)
+        self.diag = tf.add(self.diag, noise)
+
+        noise = tf.random_uniform(tf.shape(self.diag), minval=-0.01, maxval=0.01)
+        self.kernel = tf.add(self.kernel, noise)
+
+
+    self.padding = True
+    if shape_out < shape_in:
+        self.padding = False
+
+
+    if use_bias:
+      bias_initializer = tf.constant_initializer(0.0)
+      self.bias = tf.get_variable(name="bias", shape=[shape_out],
+        initializer=bias_initializer, regularizer=regularizer)
+
+  def matmul(self, input_data):
+    padding_size = (np.abs(self.size - self.shape_in))
+    paddings = ((0, 0), (padding_size, 0))
+    data = tf.pad(input_data, paddings) if self.padding else input_data
+    data = np.multiply(data, self.diag)
+    act_dct = tf.spectral.dct(data, type=2)
+    # kernel_dct = tf.spectral.dct(self.kernel, type=2)
+    # ret_mul = tf.multiply(act_dct, kernel_dct)
+    ret_mul = tf.multiply(act_dct, self.kernel)
+    ret = tf.spectral.idct(ret_mul, type=2)
+    ret = ret[..., :self.shape_out]
+    if self.use_bias:
+      ret = ret + self.bias
+    return ret
+
+
+
 class LowRankLayer:
 
   def __init__(self, rank, shape_in, shape_out, kernel_initializer=None,
