56522f47eb3f6ac0a9efbae3cb35b8101589c747
Alexandre Araujo <aaraujo001@gmail.com>
2019-02-14 18:00:17 +0100

    update run & config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,5 @@
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_test_with_random"
-WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_circulant"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_acdc"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_tensortrain"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_lowrank"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/youtube_models"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/youtube_with_tensortrain"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/youtube_lowrank"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_circulant_relu"
-# WORKDIR="/pwrwork/rech/jvd/ubx31mc/neuralnet/cifar10_toeplitz"
 
-PROJECT_PATH="/linkhome/rech/grpgen/ubx31mc/neuralnet"
+PROJECT_PATH="/home/aaraujo/neuralnet"
 CODE_PATH="${PROJECT_PATH}/code"
 CONFIG_NAME=$1
 CONFIG_PATH="${PROJECT_PATH}/config/${CONFIG_NAME}.yaml"
@@ -25,16 +15,29 @@ python3 ${CODE_PATH}/train.py \
 	--config_file=${CONFIG_PATH} \
 	--config_name=train \
 	--train_dir=${TRAIN_DIR} \
-  &>> "${LOGS_DIR}/log_train.logs" &
+        &>> "${LOGS_DIR}/log_train.logs" &
 
-export CUDA_VISIBLE_DEVICES='2,3';
+# export CUDA_VISIBLE_DEVICES='2,3';
+export CUDA_VISIBLE_DEVICES='';
 python3 ${CODE_PATH}/eval.py \
 	--config_file=${CONFIG_PATH} \
 	--config_name=eval_test \
 	--train_dir=${TRAIN_DIR} \
   &>> "${LOGS_DIR}/log_eval_test.logs" &
+
 wait
 
-python3 ${CODE_PATH}/parse_events.py \
-   --folder=${TRAIN_DIR}
+python3 ${CODE_PATH}/parse/parse_events.py \
+        --folder=${TRAIN_DIR}
+
+for attack in [ 'attack_fgsm' 'attack_pgd' 'attack_carlini' ]
+do
+  python3 ${CODE_PATH}/eval_under_attack.py \
+         	--config_file=${CONFIG_FILE} \
+  	      --config_name=${attack} \
+  	      --train_dir=${TRAIN_DIR} \
+          &>> "${LOGS_DIR}/log_${attack}.logs" & 
+  wait
+done
+
 
