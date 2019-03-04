7864fb1fa0b16bc13e368935a6babb53a6edc3d4
Alexandre Araujo <alexandre.araujo@dauphine.eu>
2019-03-04 10:25:41 +0100

    config xp adv

---
+++ xp_adv/train/run_all.sh
@@ -0,0 +1,5 @@
+bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train/sub/train_0.jt
+sleep 2
+bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train/sub/train_1.jt
+sleep 2
+bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train/sub/train_2.jt
