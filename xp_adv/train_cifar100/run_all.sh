d79772790f2ba650df33d45dc33ff8734fba9ab2
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-03-19 10:29:15 +0100

    update config & xp files

--- xp_adv/train_cifar100/run_all.sh
+++
@@ -1,5 +0,0 @@
-bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train_cifar100/sub/train_0.jt
-sleep 2
-bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train_cifar100/sub/train_1.jt
-sleep 2
-bsub < /gpfs16l/pwrhome/rech/grpgen/urz85ee/neuralnet/xp_adv/train_cifar100/sub/train_2.jt
