4064de4262555ee6153f05dd76c23fe309deb5d0
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-03-19 10:09:05 +0100

    make summary only if training and not eager execution

--- code/models/base.py
+++ code/models/base.py
@@ -16,11 +16,12 @@ class BaseModel(object):
     Returns:
       nothing
     """
-    # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
-    # session. This helps the clarity of presentation on tensorboard.
-    tensor_name = re.sub('tower_[0-9]*/', '', x.op.name)
-    tf.summary.histogram('{}/activations'.format(tensor_name), x)
-    tf.summary.scalar('{}/sparsity'.format(tensor_name), tf.nn.zero_fraction(x))
+    if self.is_training and not tf.executing_eagerly():
+      # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+      # session. This helps the clarity of presentation on tensorboard.
+      tensor_name = re.sub('tower_[0-9]*/', '', x.op.name)
+      tf.summary.histogram('{}/activations'.format(tensor_name), x)
+      tf.summary.scalar('{}/sparsity'.format(tensor_name), tf.nn.zero_fraction(x))
 
   def create_model(self, model_input, n_classes, is_training, *args, **kwargs):
     raise NotImplementedError()
