8e2e4e5168894ba414861b5c090e7b7faed85e4a
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-04-09 14:20:30 +0200

    update scripts

---
+++ sub/attacks.py
@@ -0,0 +1,88 @@
+#!/pwrlocal/pub/anaconda/py3/bin/python3
+
+import os
+import json
+import argparse
+
+attacks = "fgm pgd carlini"
+
+setup_ouessant="""
+#BSUB -J {folder_id}_attack
+#BSUB -gpu "num=4:mode=exclusive_process:mps=no:j_exclusive=yes"
+#BSUB -n 1
+#BSUB -R "span[ptile=1]"
+#BSUB -W 1200
+#BSUB -o {home}/%J.train.out
+#BSUB -e {home}/%J.train.err
+
+ml anaconda/py3 cudnn nccl
+source activate tensorflow1.12-py3
+export LD_LIBRARY_PATH={ld_library}
+"""
+
+script="""
+TRAIN_DIR="{path}/{folder}"
+LOGS_DIR=$TRAIN_DIR"_logs"
+CONFIG_FILE="$LOGS_DIR/model_flags.yaml"
+
+export CUDA_VISIBLE_DEVICES='{gpu}';
+for ATTACK in {attacks}
+do
+  python3 $PROJECTDIR/code/eval.py \\
+    --config_file=$CONFIG_FILE \\
+    --config_name=$ATTACK \\
+    --train_dir=$TRAIN_DIR \\
+    --data_dir=$DATADIR {params} \\
+    &>> "$LOGS_DIR/log_$ATTACK.logs" &
+  wait
+done
+"""
+
+def main(args):
+  global script
+
+  # check and processed attacks
+  args['attacks'] = list(set(args['attacks'].split(' ')))
+  assert set(args['attacks']).issubset(attacks.split(' ')), \
+      "attacks not found"
+  mapping = lambda x: "'attack_{}'".format(x)
+  args['attacks'] = list(map(mapping, args['attacks']))
+  args['attacks'] = ' '.join(args['attacks'])
+
+  # if params is set, overide the parameters in the config file
+  if args['params']:
+    try:
+      _ = json.loads(args['params'])
+      args['params'] = "--override {}".format(args['params'])
+    except:
+      raise ValueError("Could not parse overide parameters")
+
+  # setup ouessant job parameters 
+  if "ouessant" in args['hostname']:
+    script = setup_ouessant + script
+    args['home'] = os.environ['home']
+    args['ld_library'] = os.environ['LD_LIBRARY_PATH']
+    args['folder_id'] = args['folder'][-4:]
+
+  print(script.format(**args))
+
+
+if __name__ == '__main__':
+
+  # default path 
+  path = "{}/models".format(os.environ['WORKDIR'])
+
+  parser = argparse.ArgumentParser(
+      description='Run attacks on trained models.')
+  parser.add_argument("folder", type=str,
+                        help="Folder of the trained models.")
+  parser.add_argument("--path", type=str, default=path,
+                        help="path of the trained folder.")
+  parser.add_argument("--attacks", type=str, default=attacks,
+                        help="List of attacks to perform.")
+  parser.add_argument("--gpu", type=str, default="0",
+                        help="Set CUDA_VISIBLE_DEVICES.")
+  parser.add_argument("--params", type=str, default='',
+                        help="Parameters to override.")
+  args = parser.parse_args()
+  main(vars(args))
---
+++ sub/eval.py
@@ -0,0 +1,81 @@
+#!/pwrlocal/pub/anaconda/py3/bin/python3
+
+import os
+import json
+import argparse
+from os.path import join
+
+setup_ouessant="""
+#BSUB -J {folder_id}_eval
+#BSUB -gpu "num=4:mode=exclusive_process:mps=no:j_exclusive=yes"
+#BSUB -n 1
+#BSUB -R "span[ptile=1]"
+#BSUB -W 1200
+#BSUB -o {home}/%J.train.out
+#BSUB -e {home}/%J.train.err
+
+ml anaconda/py3 cudnn nccl
+source activate tensorflow1.12-py3
+export LD_LIBRARY_PATH={ld_library}
+"""
+
+script = """
+TRAIN_DIR="{path}/{folder}"
+LOGS_DIR="$TRAIN_DIR_logs"
+CONFIG_FILE="$LOGS_DIR/model_flags.yaml"
+
+export CUDA_VISIBLE_DEVICES='{gpu}';
+python3 $PROJECTDIR/code/eval.py \\
+  --config_file=$CONFIG_FILE \\
+  --config_name=eval_test \\
+  --train_dir=$TRAIN_DIR \\
+  --data_dir=$DATADIR {params} \\
+  &>> "$LOGS_DIR/log_eval_test.logs" &
+"""
+
+def main(args):
+  global script
+
+  # if params is set, overide the parameters in the config file
+  if args['params']:
+    try:
+      _ = json.loads(args['params'])
+      args.params = "--override {}".format(args['params'])
+    except:
+      raise ValueError("Could not parse overide parameters")
+
+  # setup ouessant job parameters 
+  if "ouessant" in args['hostname']:
+    script = setup_ouessant + script
+    args['home'] = os.environ['home']
+    args['ld_library'] = os.environ['LD_LIBRARY_PATH']
+    args['folder_id'] = args['folder'][-4:]
+
+  print(script.format(**args))
+
+
+if __name__ == '__main__':
+
+  # default path 
+  path = "{}/models".format(os.environ['WORKDIR'])
+
+  parser = argparse.ArgumentParser(
+      description='Run attacks on trained models.')
+  parser.add_argument("folder", type=str,
+                        help="Folder of the trained models.")
+  parser.add_argument("--path", type=str, default=path,
+                        help="path of the trained folder.")
+  parser.add_argument("--gpu", type=str, default="0,1,2,3",
+                        help="Set CUDA_VISIBLE_DEVICES for eval.")
+
+  # paramters for batch experiments
+  parser.add_argument("--params", type=str, default='',
+            help="Parameters to override in the config file.")
+  parser.add_argument("--name", type=str, default='',
+            help="Name of the batch experiments. Required if params is set.")
+  parser.add_argument("--id", type=str, default='',
+            help="Id of the experiment. Required if params is set.")
+  args = parser.parse_args()
+  main(vars(args))
+
+
