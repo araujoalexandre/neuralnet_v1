bf5b08393664545deaac1181c31d55cf7d2f908d
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-05-17 11:05:31 +0200

    update script for entropy

--- run_fair_entropy.sh
+++ run_fair_entropy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-MODELS_FOLDER=$1
-PARTITION=$2
+MODELS_FOLDER="models"
+PARTITION="priority"
 FOLDERS=$(find ${WORKDIR}/${MODELS_FOLDER}/* -type d | grep -v "logs" | sort)
 for FOLDER in ${FOLDERS}
 do
@@ -9,4 +9,12 @@ do
     sed "s/FOLDER/${FOLDER}/g" | sbatch
 done
 
-
+MODELS_FOLDER="models_cifar"
+PARTITION="learnfair"
+FOLDERS=$(find ${WORKDIR}/${MODELS_FOLDER}/* -type d | grep -v "logs" | sort)
+for FOLDER in ${FOLDERS}
+do
+  FOLDER=$(basename $FOLDER)
+  sed "s/PARTITION/${PARTITION}/g" compute_entropy.sh | \
+    sed "s/FOLDER/${FOLDER}/g" | sbatch
+done
