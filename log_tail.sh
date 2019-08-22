d2a57f4db4b0438419bded827158b69b5d9fb85d
Alexandre Araujo <alexandre.araujo@wavestone.com>
2019-08-22 11:16:00 +0200

    delete sh script

--- log_tail.sh
+++
@@ -1,7 +0,0 @@
-log_folder=$(find ${WORKDIR}/models/* -type d -iname "*logs" | sort | tail -n 1)
-if [ "$#" -ne 2 ]
-then
-  tail -f ${log_folder}/log_$1.logs
-else
-  tail -f ${log_folder}/log_$1.logs --lines=$2
-fi
