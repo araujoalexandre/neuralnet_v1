9c839514396675b710ba1721cf59d11bb53841c1
Alexandre Araujo <aaraujo001@gmail.com>
2020-01-08 17:27:50 +0100

    add autoaugment and update imagenet reader

--- src/tensorflow_src/dataset/readers.py
+++ src/tensorflow_src/dataset/readers.py
@@ -8,6 +8,8 @@ from tensorflow.io import gfile
 from tensorflow.python.data.ops import multi_device_iterator_ops
 from tensorflow.contrib.data.python.ops import threadpool
 
+from . import autoaugment
+
 
 def normalize_image(image):
   # rescale between [-1, 1]
@@ -510,165 +512,147 @@ class IMAGENETReader(BaseReader):
 
     return features['image/encoded'], label, bbox, features['image/class/text']
 
-  def _decode_jpeg(self, image_buffer, scope=None):
-    """Decode a JPEG string into one 3-D float image Tensor.
+  def distorted_bounding_box_crop(self,
+                                  image_bytes,
+                                  bbox,
+                                  min_object_covered=0.1,
+                                  aspect_ratio_range=(0.75, 1.33),
+                                  area_range=(0.05, 1.0),
+                                  max_attempts=100,
+                                  scope=None):
+    """Generates cropped_image using one of the bboxes randomly distorted.
+    See `tf.image.sample_distorted_bounding_box` for more documentation.
     Args:
-      image_buffer: scalar string Tensor.
-      scope: Optional scope for name_scope.
+      image_bytes: `Tensor` of binary image data.
+      bbox: `Tensor` of bounding boxes arranged `[1, num_boxes, coords]`
+          where each coordinate is [0, 1) and the coordinates are arranged
+          as `[ymin, xmin, ymax, xmax]`. If num_boxes is 0 then use the whole
+          image.
+      min_object_covered: An optional `float`. Defaults to `0.1`. The cropped
+          area of the image must contain at least this fraction of any bounding
+          box supplied.
+      aspect_ratio_range: An optional list of `float`s. The cropped area of the
+          image must have an aspect ratio = width / height within this range.
+      area_range: An optional list of `float`s. The cropped area of the image
+          must contain a fraction of the supplied image within in this range.
+      max_attempts: An optional `int`. Number of attempts at generating a cropped
+          region of the image of the specified constraints. After `max_attempts`
+          failures, return the entire image.
+      scope: Optional `str` for name scope.
     Returns:
-      3-D float Tensor with values ranging from [0, 1).
+      cropped image `Tensor`
     """
-    with tf.name_scope(values=[image_buffer], name=scope,
-                       default_name='decode_jpeg'):
-      # Decode the string as an RGB JPEG.
-      # Note that the resulting image contains an unknown height and width
-      # that is set dynamically by decode_jpeg. In other words, the height
-      # and width of image is unknown at compile-time.
-      image = tf.image.decode_jpeg(image_buffer, channels=3)
-
-      # After this point, all image pixels reside in [0,1)
-      # until the very end, when they're rescaled to (-1, 1).  The various
-      # adjust_* ops all require this range for dtype float.
-      image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-      return image
-
-  def _eval(self, image, scope=None):
-    """Prepare one image for evaluation.
-    Args:
-      image: 3-D float Tensor
-      height: integer
-      width: integer
-      scope: Optional scope for name_scope.
-    Returns:
-      3-D float Tensor of prepared image.
-    """
-    height, width = self.height, self.width
-    with tf.name_scope(values=[image, height, width], name=scope,
-                       default_name='eval_image'):
-      # Crop the central region of the image with an area containing 87.5% of
-      # the original image.
-      image = tf.image.central_crop(image, central_fraction=0.875)
-
-      # Resize the image to the original height and width.
-      image = tf.expand_dims(image, 0)
-      image = tf.image.resize_bilinear(image, [height, width],
-                                       align_corners=False)
-      image = tf.squeeze(image, [0])
-      return image
-
-  def _distort_color(self, image, scope=None):
-    """Distort the color of the image.
-    Each color distortion is non-commutative and thus ordering of the color ops
-    matters. We randomly permute the ordering of the color ops.
-    Args:
-      image: Tensor containing single image.
-      scope: Optional scope for name_scope.
-    Returns:
-      color-distorted image
-    """
-    with tf.name_scope(values=[image], name=scope, default_name='distort_color'):
-
-      def color_ordering_1(image):
-        image = tf.image.random_brightness(image, max_delta=32. / 255.)
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_hue(image, max_delta=0.2)
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        return image
-
-      def color_ordering_2(image):
-        image = tf.image.random_brightness(image, max_delta=32. / 255.)
-        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-        image = tf.image.random_hue(image, max_delta=0.2)
-        return image
-
-      rand = tf.random_uniform([1], 0, 2, tf.int32)
-      image = tf.cond(tf.equal(rand[0], 1),
-                      lambda: color_ordering_1(image),
-                      lambda: color_ordering_2(image))
-
-      # The random_* ops do not necessarily clamp.
-      image = tf.clip_by_value(image, 0.0, 1.0)
-      return image
-
-  def _data_augmentation(self, image, bbox, scope=None):
-    """Distort one image for training a network.
-    Distorting images provides a useful technique for augmenting the data
-    set during training in order to make the network invariant to aspects
-    of the image that do not effect the label.
-    Args:
-      image: 3-D float Tensor of image
-      bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
-        where each coordinate is [0, 1) and the coordinates are arranged
-        as [ymin, xmin, ymax, xmax].
-      scope: Optional scope for name_scope.
-    Returns:
-      3-D float Tensor of distorted image used for training.
-    """
-    height, width = self.height, self.width
-    with tf.name_scope(values=[image, height, width, bbox], name=scope,
-                       default_name='distort_image'):
-      # Each bounding box has shape [1, num_boxes, box coords] and
-      # the coordinates are ordered [ymin, xmin, ymax, xmax].
-
-      # Display the bounding box in the first thread only.
-      if self.summary_verbosity >= 3:
-        image_with_box = tf.image.draw_bounding_boxes(tf.expand_dims(image, 0),
-                                                      bbox)
-        tf.summary.image('image_with_bounding_boxes', image_with_box)
-
-      # A large fraction of image datasets contain a human-annotated bounding
-      # box delineating the region of the image containing the object of interest.
-      # We choose to create a new bounding box for the object which is a randomly
-      # distorted version of the human-annotated bounding box that obeys an 
-      # allowed range of aspect ratios, sizes and overlap with the human-annotated
-      # bounding box. If no box is supplied, then we assume the bounding box is
-      # the entire image.
+    with tf.name_scope(scope, 'distorted_bounding_box_crop', [image_bytes, bbox]):
+      shape = tf.image.extract_jpeg_shape(image_bytes)
       sample_distorted_bounding_box = tf.image.sample_distorted_bounding_box(
-          tf.shape(image),
+          shape,
           bounding_boxes=bbox,
-          min_object_covered=0.1,
-          aspect_ratio_range=[0.75, 1.33],
-          area_range=[0.05, 1.0],
-          max_attempts=100,
+          min_object_covered=min_object_covered,
+          aspect_ratio_range=aspect_ratio_range,
+          area_range=area_range,
+          max_attempts=max_attempts,
           use_image_if_no_bounding_boxes=True)
-      bbox_begin, bbox_size, distort_bbox = sample_distorted_bounding_box
-      if self.summary_verbosity >= 3:
-        image_with_distorted_box = tf.image.draw_bounding_boxes(
-            tf.expand_dims(image, 0), distort_bbox)
-        tf.summary.image('images_with_distorted_bounding_box',
-                         image_with_distorted_box)
-
+      bbox_begin, bbox_size, _ = sample_distorted_bounding_box
+  
       # Crop the image to the specified bounding box.
-      distorted_image = tf.slice(image, bbox_begin, bbox_size)
-
-      # This resizing operation may distort the images because the aspect
-      # ratio is not respected. We select a resize method randomly 
-      # Note that ResizeMethod contains 4 enumerated resizing methods.
-      # resize_method = tf.random_uniform([1], 0, 4, tf.int32)
-      # distorted_image = tf.image.resize_images(distorted_image, [height, width],
-      #                                          method=resize_method[0])
-
-      distorted_image = tf.image.resize_images(distorted_image, [height, width],
-                                               method=0)
-
-      # Restore the shape since the dynamic slice based upon the bbox_size loses
-      # the third dimension.
-      distorted_image.set_shape([height, width, 3])
-      if self.summary_verbosity >= 3:
-        tf.summary.image('cropped_resized_image',
-                         tf.expand_dims(distorted_image, 0))
-
-      # Randomly flip the image horizontally.
-      distorted_image = tf.image.random_flip_left_right(distorted_image)
-
-      # Randomly distort the colors.
-      distorted_image = self._distort_color(distorted_image)
-
-      if self.summary_verbosity >= 3:
-        tf.summary.image('final_distorted_image',
-                         tf.expand_dims(distorted_image, 0))
-      return distorted_image
+      offset_y, offset_x, _ = tf.unstack(bbox_begin)
+      target_height, target_width, _ = tf.unstack(bbox_size)
+      crop_window = tf.stack([offset_y, offset_x, target_height, target_width])
+      image = tf.image.decode_and_crop_jpeg(image_bytes, crop_window, channels=3)
+  
+      return image
+  
+  def _at_least_x_are_equal(self, a, b, x):
+    """At least `x` of `a` and `b` `Tensors` are equal."""
+    match = tf.equal(a, b)
+    match = tf.cast(match, tf.int32)
+    return tf.greater_equal(tf.reduce_sum(match), x)
+  
+  def _decode_and_random_crop(self, image_bytes, image_size):
+    """Make a random crop of image_size."""
+    bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf.float32, shape=[1, 1, 4])
+    image = self.distorted_bounding_box_crop(
+        image_bytes,
+        bbox,
+        min_object_covered=0.1,
+        aspect_ratio_range=(3. / 4, 4. / 3.),
+        area_range=(0.08, 1.0),
+        max_attempts=10,
+        scope=None)
+    original_shape = tf.image.extract_jpeg_shape(image_bytes)
+    bad = self._at_least_x_are_equal(original_shape, tf.shape(image), 3)
+  
+    image = tf.cond(
+        bad,
+        lambda: self._decode_and_center_crop(image_bytes, image_size),
+        lambda: tf.image.resize_bicubic([image],  # pylint: disable=g-long-lambda
+                                        [image_size, image_size])[0])
+  
+    return image
+  
+  def _decode_and_center_crop(self, image_bytes, image_size):
+    """Crops to center of image with padding then scales image_size."""
+    shape = tf.image.extract_jpeg_shape(image_bytes)
+    image_height = shape[0]
+    image_width = shape[1]
+  
+    padded_center_crop_size = tf.cast(
+      ((image_size / (image_size + 32)) *
+         tf.cast(tf.minimum(image_height, image_width), tf.float32)),
+        tf.int32)
+  
+    offset_height = ((image_height - padded_center_crop_size) + 1) // 2
+    offset_width = ((image_width - padded_center_crop_size) + 1) // 2
+    crop_window = tf.stack([offset_height, offset_width,
+                            padded_center_crop_size, padded_center_crop_size])
+    image = tf.image.decode_and_crop_jpeg(image_bytes, crop_window, channels=3)
+    image = tf.image.resize_bicubic([image], [image_size, image_size])[0]
+    return image
+  
+  def _flip(self, image):
+    """Random horizontal image flip."""
+    image = tf.image.random_flip_left_right(image)
+    return image
+  
+  def preprocess_for_train(self, image_bytes):
+    """Preprocesses the given image for training.
+    Args:
+      image_bytes: `Tensor` representing an image binary of arbitrary size.
+      image_size: image size.
+    Returns:
+      A preprocessed image `Tensor`.
+    """
+    image_size = self.image_size
+    image = self._decode_and_random_crop(image_bytes, image_size)
+    image = self._flip(image)
+    image = tf.reshape(image, [image_size, image_size, 3])
+  
+    image = tf.image.convert_image_dtype(
+        image, dtype=tf.bfloat16 if self.use_bfloat16 else tf.float32)
+  
+    if self.augmentation_strategy == 'autoaugment':
+      logging.info('Apply AutoAugment policy')
+      input_image_type = image.dtype
+      image = tf.clip_by_value(image, 0.0, 255.0)
+      image = tf.cast(image, dtype=tf.uint8)
+      image = autoaugment.distort_image_with_autoaugment(image, 'v0')
+      image = tf.cast(image, dtype=input_image_type)
+    return image
+  
+  def preprocess_for_eval(self, image_bytes):
+    """Preprocesses the given image for evaluation.
+    Args:
+      image_bytes: `Tensor` representing an image binary of arbitrary size.
+      use_bfloat16: `bool` for whether to use bfloat16.
+    Returns:
+      A preprocessed image `Tensor`.
+    """
+    image_size = self.image_size
+    image = self._decode_and_center_crop(image_bytes, image_size)
+    image = tf.reshape(image, [image_size, image_size, 3])
+    image = tf.image.convert_image_dtype(
+        image, dtype=tf.bfloat16 if use_bfloat16 else tf.float32)
+    return image
 
   def _image_preprocessing(self, image_buffer, bbox):
     """Decode and preprocess one image for evaluation or training.
@@ -677,14 +661,13 @@ class IMAGENETReader(BaseReader):
     Returns:
       3D Tensor containing an appropriately scaled image
     """
-    # Decode the string as an RGB JPEG.
-    image = self._decode_jpeg(image_buffer)
     if self.is_training:
-      image = self._data_augmentation(image, bbox)
+      image = self.preprocess_for_train(image_buffer)
     else:
-      image = self._eval(image)
-    image = normalize_image(image)
+      image = self.preprocess_for_eval(image_buffer)
     return image
+
+
 
 
 class ImagenetScatteringReader(BaseReader):
