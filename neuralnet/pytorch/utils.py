8fee4563c1ae5f356e6d11c9a1a101f9210f2570
Alexandre Araujo <aaraujo001@gmail.com>
2020-06-21 11:05:18 +0200

    add EMA class

--- neuralnet/pytorch/utils.py
+++ neuralnet/pytorch/utils.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 import multiprocessing
 
 import torch
@@ -174,3 +175,29 @@ class CrossEntropyLabelSmooth(torch.nn.Module):
     elif self.reduction == 'sum':
       loss = loss.sum()
     return loss
+
+
+class EMA:
+
+  def __init__(self, mu):
+    self.mu = mu
+    self.shadow = {}
+
+  def state_dict(self):
+    return copy.deepcopy(self.shadow)
+
+  def __len__(self):
+    return len(self.shadow)
+
+  def __call__(self, module, step=None):
+    if step is None:
+      mu = self.mu
+    else:
+      # see: tensorflow doc ExponentialMovingAverage
+      mu = min(self.mu, (1. + step) / (10 + step))
+    for name, x in module.state_dict().items():
+      if name in self.shadow:
+        new_average = (1.0 - mu) * x + mu * self.shadow[name]
+        self.shadow[name] = new_average.clone()
+      else:
+        self.shadow[name] = x.clone()
