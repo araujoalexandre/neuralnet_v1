81b24469fcffdb3eb1e5dd7306c973adf41e0ce2
Alexandre Araujo <aaraujo001@gmail.com>
2020-06-21 19:44:39 +0200

    update train files

--- neuralnet/pytorch/train.py
+++ neuralnet/pytorch/train.py
@@ -6,6 +6,7 @@ import pprint
 import socket
 import logging
 import warnings
+import glob
 from os import mkdir
 from os.path import join
 from os.path import exists
@@ -15,6 +16,7 @@ from . import utils
 from .models import model_config
 from .lipschitz import LipschitzRegularization
 from .rmsprop import RMSpropTF
+from .utils import GradualWarmupScheduler
 
 from .dataset.readers import readers_config
 
@@ -23,13 +25,9 @@ import torch
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import torch.distributed as dist
+from torch.nn import SyncBatchNorm
 from torch.optim import lr_scheduler
 from torch.nn.parallel import DistributedDataParallel
-
-try:
-  from warmup_scheduler import GradualWarmupScheduler
-except:
-  pass
 
 
 def get_scheduler(optimizer, lr_scheduler, lr_scheduler_params):
@@ -53,18 +51,16 @@ def get_scheduler(optimizer, lr_scheduler, lr_scheduler_params):
   elif lr_scheduler == 'cyclic_lr':
     scheduler = torch.optim.lr_scheduler.CyclicLR(
       optimizer, **lr_scheduler_params)
-  elif lr_scheduler == 'lambda_lr_warmup':
-    warmup_epoch = lr_scheduler_params['warmup_epoch']
-    warmup_multiplier = lr_scheduler_params['warmup_multiplier']
-    lr_lambda = lambda x: 0.97 ** int((x + warmup_epoch) / 2.4)
-    after_scheduler = torch.optim.lr_scheduler.LambdaLR(
-        optimizer, lr_lambda=lr_lambda)
-    scheduler = GradualWarmupScheduler(
-        optimizer,
-        multiplier=warmup_epoch,
-        total_epoch=warmup_multiplier,
-        after_scheduler=after_scheduler
-    )
+  elif lr_scheduler == 'lambda_lr':
+    gamma = lr_scheduler_params['gamma']
+    decay_every_epoch = lr_scheduler_params['decay_every_epoch']
+    warmup = lr_scheduler_params['warmup']
+    def lambda_lr(epoch):
+      if epoch < warmup:
+        return epoch / warmup
+      return gamma ** (int(epoch // decay_every_epoch))
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+      optimizer, lr_lambda=lambda_lr)
   else:
     raise ValueError("scheduler was not recognized")
   return scheduler
@@ -82,7 +78,8 @@ def get_optimizer(optimizer, opt_args, init_lr, weight_decay, params):
     opt = torch.optim.Adam(
       params, lr=init_lr, weight_decay=weight_decay, **opt_args)
   elif optimizer == 'rmsproptf':
-    opt = RMSpropTF(params, lr=init_lr, weight_decay=weight_decay, **opt_args)
+    # we compute the l2 loss manualy without bn params
+    opt = RMSpropTF(params, lr=init_lr, weight_decay=0, **opt_args)
   else:
     raise ValueError("Optimizer was not recognized")
   return opt
@@ -99,12 +96,6 @@ class Trainer:
 
     # Setup logging & log the version.
     global_utils.setup_logging(params.logging_verbosity)
-    logging.info("PyTorch version: {}.".format(torch.__version__))
-    logging.info("Hostname: {}.".format(socket.gethostname()))
-
-    # print self.params parameters
-    pp = pprint.PrettyPrinter(indent=2, compact=True)
-    logging.info(pp.pformat(params.values()))
 
     self.job_name = self.params.job_name  # "" for local training
     self.is_distributed = bool(self.job_name)
@@ -118,6 +109,16 @@ class Trainer:
     else:
       self.batch_size = self.params.batch_size
 
+    # print self.params parameters
+    if self.start_new_model and self.local_rank == 0:
+      pp = pprint.PrettyPrinter(indent=2, compact=True)
+      logging.info(pp.pformat(params.values()))
+
+    if self.local_rank == 0:
+      logging.info("PyTorch version: {}.".format(torch.__version__))
+      logging.info("NCCL Version {}".format(torch.cuda.nccl.version()))
+      logging.info("Hostname: {}.".format(socket.gethostname()))
+
     if self.is_distributed:
       self.num_nodes = len(params.worker_hosts.split(';'))
       self.world_size = self.num_nodes * self.num_gpus
@@ -126,11 +127,11 @@ class Trainer:
         backend='nccl', init_method='env://',
         timeout=datetime.timedelta(seconds=30))
       if self.local_rank == 0:
-        logging.info('world size={}'.format(self.world_size))
-      logging.info('Distributed init done, local_rank={}, rank={}'.format(
-        self.local_rank, self.rank))
+        logging.info('World Size={} => Total batch size {}'.format(
+          self.world_size, self.batch_size*self.world_size))
       self.is_master = bool(self.rank == 0)
     else:
+      self.world_size = 1
       self.is_master = True
 
     # create a mesage builder for logging
@@ -142,22 +143,51 @@ class Trainer:
     self.model = model_config.get_model_config(
         self.params.model, self.params.dataset, self.params,
         self.reader.n_classes, is_training=True)
-    if not params.job_name:
-      self.model = torch.nn.DataParallel(self.model)
-      self.model = self.model.cuda()
-    else:
-      torch.cuda.set_device(params.local_rank)
-      self.model = self.model.cuda()
-      i = params.local_rank
-      self.model = DistributedDataParallel(
-        self.model, device_ids=[i], output_device=i)
-      logging.info('model defined with DistributedDataParallel')
+    # define DistributedDataParallel job
+    self.model = SyncBatchNorm.convert_sync_batchnorm(self.model)
+    torch.cuda.set_device(params.local_rank)
+    self.model = self.model.cuda()
+    i = params.local_rank
+    self.model = DistributedDataParallel(
+      self.model, device_ids=[i], output_device=i)
+    if self.local_rank == 0:
+      logging.info('Model defined with DistributedDataParallel')
+
+    # define set for saved ckpt
+    self.saved_ckpts = set([0])
+
+    # define optimizer
+    self.optimizer = get_optimizer(
+                       self.params.optimizer,
+                       self.params.optimizer_params,
+                       self.params.init_learning_rate,
+                       self.params.weight_decay,
+                       self.model.parameters())
+
+    # define learning rate scheduler
+    self.scheduler = get_scheduler(
+      self.optimizer, self.params.lr_scheduler,
+      self.params.lr_scheduler_params)
+
+    # if start_new_model is False, we restart training
+    if not self.start_new_model:
+      if self.local_rank == 0:
+        logging.info('Restarting training...')
+      self.load_state()
 
     # define Lipschitz Reg module
-    self.lipschitz_reg = LipschitzRegularization(self.model, self.params)
+    self.lipschitz_reg = LipschitzRegularization(
+      self.model, self.params, self.reader, self.local_rank)
+
+    # exponential moving average
+    self.ema = None
+    if getattr(self.params, 'ema', False) > 0:
+      self.ema = utils.EMA(self.params.ema)
 
     # if adversarial training, create the attack class
     if self.params.adversarial_training:
+      if self.local_rank == 0:
+        logging.info('Adversarial Training')
       attack_params = self.params.adversarial_training_params
       self.attack = utils.get_attack(
                       self.model,
@@ -165,13 +195,29 @@ class Trainer:
                       self.params.adversarial_training_name,
                       attack_params)
 
+  def load_state(self):
+    # load last checkpoint
+    checkpoints = glob.glob(join(self.train_dir, "model.ckpt-*.pth"))
+    get_model_id = lambda x: int(x.strip('.pth').strip('model.ckpt-'))
+    checkpoints = sorted(
+      [ckpt.split('/')[-1] for ckpt in checkpoints], key=get_model_id)
+    path_last_ckpt = join(self.train_dir, checkpoints[-1])
+    self.checkpoint = torch.load(path_last_ckpt)
+    self.model.load_state_dict(self.checkpoint['model_state_dict'])
+    self.optimizer.load_state_dict(self.checkpoint['optimizer_state_dict'])
+    self.scheduler.load_state_dict(self.checkpoint['scheduler'])
+    self.saved_ckpts.add(self.checkpoint['epoch'])
+    epoch = self.checkpoint['epoch']
+    if self.local_rank == 0:
+      logging.info('Loading checkpoint {}'.format(checkpoints[-1]))
+
   def run(self):
     """Performs training on the currently defined Tensorflow graph.
     """
     # reset the training directory if start_new_model is True
     if self.is_master and self.start_new_model and exists(self.train_dir):
       global_utils.remove_training_directory(self.train_dir)
-    if self.is_master:
+    if self.is_master and self.start_new_model:
       mkdir(self.train_dir)
 
     if self.params.torch_random_seed is not None:
@@ -195,22 +241,24 @@ class Trainer:
     if self.params.lb_smooth == 0:
       self.criterion = torch.nn.CrossEntropyLoss().cuda()
     else:
+      if self.local_rank == 0:
+        logging.info("Using CrossEntropyLoss with label smooth {}.".format(
+          self.params.lb_smooth))
       self.criterion = utils.CrossEntropyLabelSmooth(
         self.reader.n_classes, self.params.lb_smooth)
 
-    self.optimizer = get_optimizer(
-                       self.params.optimizer,
-                       self.params.optimizer_params,
-                       self.params.init_learning_rate,
-                       self.params.weight_decay,
-                       self.model.parameters())
+    # if start_new_model is True, global_step = 0
+    # else we get global step from checkpoint
+    if self.start_new_model:
+      start_epoch = 0
+      global_step = 0
+    else:
+      start_epoch = self.checkpoint['epoch']
+      global_step = self.checkpoint['global_step']
 
-    scheduler = get_scheduler(
-      self.optimizer, self.params.lr_scheduler,
-      self.params.lr_scheduler_params)
-
-    global_step = 0
     data_loader, sampler = self.reader.load_dataset()
+    if sampler is not None:
+      assert sampler.num_replicas == self.world_size
 
     batch_size = self.batch_size
     if self.is_distributed:
@@ -219,13 +267,13 @@ class Trainer:
       n_files = self.reader.n_train_files
 
     if self.local_rank == 0:
+      logging.info("Number of files on worker: {}".format(n_files))
       logging.info("Start training")
-      logging.info("NCCL Version {}".format(torch.cuda.nccl.version()))
 
     profile_enabled = False
-    for i in range(self.params.num_epochs):
+    for epoch_id in range(start_epoch, self.params.num_epochs):
       if self.is_distributed:
-        sampler.set_epoch(i)
+        sampler.set_epoch(epoch_id)
       for n_batch, data in enumerate(data_loader):
         epoch = (int(global_step) * batch_size) / n_files
         with torch.autograd.profiler.profile(
@@ -233,25 +281,35 @@ class Trainer:
           self._training(data, epoch, global_step)
         if profile_enabled:
           logging.info(prof.key_averages().table(sort_by="self_cpu_time_total"))
-        self.save_ckpt(global_step, epoch)
+          # prof.export_chrome_trace(join(
+          #   self.train_dir+'_logs', 'trace_{}.json'.format(global_step)))
+        self.save_ckpt(global_step, epoch_id)
         global_step += 1
-      scheduler.step()
-    self.save_ckpt(global_step, epoch, final=True)
+      self.scheduler.step()
+    self.save_ckpt(global_step, epoch_id, final=True)
     logging.info("Done training -- epoch limit reached.")
 
   def save_ckpt(self, step, epoch, final=False):
     """Save ckpt in train directory."""
-    if (epoch % self.params.save_checkpoint_epochs == 0
-         and self.is_master) or (final and self.is_master):
+    freq_ckpt_epochs = self.params.save_checkpoint_epochs
+    if (epoch % freq_ckpt_epochs == 0 and self.is_master \
+        and epoch not in self.saved_ckpts) \
+         or (final and self.is_master):
+      ckpt_name = "model.ckpt-{}.pth".format(step)
+      ckpt_path = join(self.train_dir, ckpt_name)
+      if exists(ckpt_path): return 
+      self.saved_ckpts.add(epoch)
       state = {
         'epoch': epoch,
         'global_step': step,
         'model_state_dict': self.model.state_dict(),
         'optimizer_state_dict': self.optimizer.state_dict(),
+        'scheduler': self.scheduler.state_dict()
       }
-      ckpt_name = "model.ckpt-{}.pth".format(step)
+      if self.ema is not None:
+        state['ema'] = self.ema.state_dict()
       logging.info("Saving checkpoint '{}'.".format(ckpt_name))
-      torch.save(state, join(self.train_dir, ckpt_name))
+      torch.save(state, ckpt_path)
 
 
   def _training(self, data, epoch, step):
@@ -262,14 +320,21 @@ class Trainer:
     labels = labels.cuda(non_blocking=True)
 
     if self.params.adversarial_training:
-      inputs = self.attack.perturb(inputs, labels)
+      inputs = self.attack.perturb(inputs)
 
     outputs = self.model(inputs)
-    self.optimizer.zero_grad()
-
     loss = self.criterion(outputs, labels.cuda())
-    lip_loss = self.lipschitz_reg.get_lip_reg(epoch, self.model)
+
+    with torch.autograd.profiler.record_function("lipreg"):
+      lip_loss = self.lipschitz_reg.get_lip_reg(epoch, self.model)
     total_loss = loss + lip_loss
+    if self.params.optimizer == 'rmsproptf':
+      params_without_bn = [p for n, p in self.model.named_parameters() \
+                           if not ('_bn' in n or '.bn' in n)]
+      total_loss += self.params.weight_decay * (1./2.) * \
+          sum([torch.sum(p**2) for p in params_without_bn])
+
+    self.optimizer.zero_grad()
     total_loss.backward()
 
     if self.params.gradient_clip_by_norm:
@@ -282,6 +347,20 @@ class Trainer:
     self.optimizer.step()
     seconds_per_batch = time.time() - batch_start_time
     examples_per_second = self.batch_size / seconds_per_batch
+    examples_per_second *= self.world_size
+    
+    if step == 10 and self.is_master:
+      nb_imgs_to_process = self.reader.n_train_files * self.params.num_epochs
+      total_seconds = nb_imgs_to_process / examples_per_second
+      nb_days = total_seconds // 86400
+      nb_hours = (total_seconds % 86400) / 3600
+      logging.info(
+        'Approximated training time: {:.0f} days and {:.1f} hours'.format(
+          nb_days, nb_hours))
+
+    # update ema
+    if self.ema is not None:
+      self.ema(self.model, step)
 
     local_rank = self.local_rank
     to_print = step % self.params.frequency_log_steps == 0
--- neuralnet/train.py
+++ neuralnet/train.py
@@ -16,20 +16,16 @@ flags.DEFINE_string("data_dir", "",
                     "Name of the data directory")
 flags.DEFINE_bool("start_new_model", True,
                   "Start training a new model or restart an existing one.")
-flags.DEFINE_enum('job_name', '', ('ps', 'worker', 'controller', ''),
-                  'One of "ps", "worker", "controller", "".  Empty for local '
-                  'training')
-flags.DEFINE_integer('local_rank', 0, 'Define local rank.')
-flags.DEFINE_string('ps_hosts', '', 'Comma-separated list of target hosts')
-flags.DEFINE_string('worker_hosts', '', 'Comma-separated list of target hosts')
-flags.DEFINE_string('controller_host', None, 'optional controller host')
-flags.DEFINE_string('master_host', '', 'ip/hostname of the master')
-flags.DEFINE_string('master_port', '', 'port of the master')
-flags.DEFINE_integer('task_index', 0, 'Index of task within the job')
-flags.DEFINE_string('horovod_device', '', 'Device to do Horovod all-reduce on: '
-                    'empty (default), cpu or gpu. Default with utilize GPU if '
-                    'Horovod was compiled with the HOROVOD_GPU_ALLREDUCE '
-                    'option, and CPU otherwise.')
+flags.DEFINE_enum("job_name", "", ("ps", "worker", "controller", ""),
+                  "Type of job 'ps', 'worker', 'controller', ''.")
+flags.DEFINE_integer("n_gpus", 4, "Number of GPUs to use.")
+flags.DEFINE_integer("local_rank", 0, "Define local rank.")
+flags.DEFINE_string("ps_hosts", "", "Comma-separated list of target hosts.")
+flags.DEFINE_string("worker_hosts", "", "Comma-separated list of target hosts.")
+flags.DEFINE_string("controller_host", None, "optional controller host.")
+flags.DEFINE_string("master_host", "", "ip/hostname of the master.")
+flags.DEFINE_string("master_port", "", "port of the master.")
+flags.DEFINE_integer("task_index", 0, "Index of task within the job")
 flags.DEFINE_string("backend", "tensorflow",
                     "Wheather run tensorflow model of Pytorch.")
 
@@ -43,6 +39,7 @@ def main(_):
   params.train_dir = FLAGS.train_dir
   params.data_dir = FLAGS.data_dir
   params.start_new_model = FLAGS.start_new_model
+  params.num_gpus = FLAGS.n_gpus
   params.job_name = FLAGS.job_name
   params.local_rank = FLAGS.local_rank
   params.ps_hosts = FLAGS.ps_hosts
@@ -51,11 +48,10 @@ def main(_):
   params.master_host = FLAGS.master_host
   params.master_port = FLAGS.master_port
   params.task_index = FLAGS.task_index
-  params.horovod_device = FLAGS.horovod_device
 
-  if FLAGS.backend.lower() in ('tensorflow', 'tf'):
+  if FLAGS.backend.lower() == "tensorflow":
     from neuralnet.tensorflow.train import Trainer
-  elif FLAGS.backend.lower() in ('pytorch', 'py', 'torch'):
+  elif FLAGS.backend.lower() == "pytorch":
     from neuralnet.pytorch.train import Trainer
   else:
     raise ValueError(
