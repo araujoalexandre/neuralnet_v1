2c07044c292a6047acd6ca5846c0ca569b457a9b
Alexandre Araujo <aaraujo001@gmail.com>
2020-06-21 10:58:01 +0200

    refactoring eval script

--- neuralnet/pytorch/eval.py
+++ neuralnet/pytorch/eval.py
@@ -26,7 +26,6 @@ class Evaluator:
 
   def __init__(self, params):
 
-
     # Set up environment variables before doing any other global initialization to
     # make sure it uses the appropriate environment variables.
     utils.set_default_param_values_and_env_vars(params)
@@ -98,75 +97,71 @@ class Evaluator:
                       attack_params)
 
 
-
   def run(self):
-    """Evaluate a model every self.params.eval_interval_secs.
-
-    Returns:
-      Dictionary containing eval statistics. Currently returns an empty
-      dictionary.
-
-    Raises:
-      ValueError: If self.params.train_dir is unspecified.
-    """
+    """Run evaluation of model or eval under attack"""
     logging.info("Building evaluation graph")
+    if not self.params.eval_under_attack:
+      self._run_eval()
+      logging.info("Done evaluation -- number of eval reached.")
+    else:
+      self._run_under_attack()
+      logging.info('Evalution under attack done.')
 
+
+  def load_ckpt(self, path):
+    checkpoint = torch.load(path)
+    global_step = checkpoint['global_step']
+    epoch = checkpoint['epoch']
+    if 'ema' in checkpoint.keys():
+      logging.info("Loading ema state dict.")
+      self.model.load_state_dict(checkpoint['ema'])
+    else:
+      self.model.load_state_dict(checkpoint['model_state_dict'])
+    self.model.eval()
+    return global_step, epoch
+
+
+  def _run_eval(self):
     # those variables are updated in eval_loop
     self.best_global_step = None
     self.best_accuracy = None
+    ckpts = global_utils.get_list_checkpoints(
+      self.train_dir, backend='pytorch')
+    for ckpt in ckpts[::-1]:
+      # remove first checkpoint model.ckpt-0
+      if 'model.ckpt-0' in ckpt: continue
+      logging.info(
+        "Loading checkpoint for eval: {}".format(ckpt))
+      global_step, epoch = self.load_ckpt(ckpt)
+      self.eval_loop(global_step, epoch)
 
-    if self.params.eval_under_attack:
-      # normal evaluation has already been done
-      # we get the best checkpoint of the model
-      best_checkpoint, global_step = \
-          global_utils.get_best_checkpoint(
-            self.logs_dir, backend='pytorch')
-
-      checkpoint = torch.load(best_checkpoint)
-      global_step = checkpoint['global_step']
-      epoch = checkpoint['epoch']
-      self.model.load_state_dict(checkpoint['model_state_dict'])
-      self.model.eval()
-      self.eval_attack(global_step, epoch)
-
-      # save results
-      path = join(self.logs_dir, "attacks_score.txt")
+    if self.best_global_step is not None and self.best_accuracy is not None:
+      path = join(self.logs_dir, "best_accuracy.txt")
       with open(path, 'a') as f:
-        f.write("{}\n".format(self.attack.__class__.__name__))
-        if self.params.eot:
-          f.write("eot sample {}, {}\n".format(self.eot_samples,
-                                         json.dumps(self.params.attack_params)))
-        else:
-          f.write("{}\n".format(json.dumps(self.params.attack_params)))
-        f.write("{:.5f}\n\n".format(self.best_accuracy))
-      logging.info('Evalution under attack done.')
-      return
+        f.write("{}\t{:.4f}\n".format(
+          self.best_global_step, self.best_accuracy))
 
-    # make global evaluation
-    else:
-      ckpts = global_utils.get_list_checkpoints(
-        self.train_dir, backend='pytorch')
-      for ckpt in ckpts:
-        # remove first checkpoint model.ckpt-0
-        if 'model.ckpt-0' in ckpt:
-          continue
-        logging.info(
-          "Loading checkpoint for eval: {}".format(ckpt))
-        # Restores from checkpoint
-        checkpoint = torch.load(ckpt)
-        global_step = checkpoint['global_step']
-        epoch = checkpoint['epoch']
-        self.model.load_state_dict(checkpoint['model_state_dict'])
-        self.model.eval()
-        self.eval_loop(global_step, epoch)
 
-      if self.best_global_step is not None and self.best_accuracy is not None:
-        path = join(self.logs_dir, "best_accuracy.txt")
-        with open(path, 'a') as f:
-          f.write("{}\t{:.4f}\n".format(
-            self.best_global_step, self.best_accuracy))
+  def _run_under_attack(self):
+    # normal evaluation has already been done
+    # we get the best checkpoint of the model
+    best_checkpoint, global_step = \
+        global_utils.get_best_checkpoint(
+          self.logs_dir, backend='pytorch')
+    logging.info("Loading '{}'".format(best_checkpoint.split('/')[-1]))
+    global_step, epoch = self.load_ckpt(best_checkpoint)
+    self.eval_attack(global_step, epoch)
 
-    logging.info("Done evaluation -- number of eval reached.")
+    # save results
+    path = join(self.logs_dir, "attacks_score.txt")
+    with open(path, 'a') as f:
+      f.write("{}\n".format(self.attack.__class__.__name__))
+      if self.params.eot:
+        f.write("eot sample {}, {}\n".format(self.eot_samples,
+                                       json.dumps(self.params.attack_params)))
+      else:
+        f.write("{}\n".format(json.dumps(self.params.attack_params)))
+      f.write("{:.5f}\n\n".format(self.best_accuracy))
 
 
   def eval_loop(self, global_step, epoch):
@@ -196,6 +191,7 @@ class Evaluator:
       accuracy = running_accuracy / running_inputs
       loss = running_loss / (batch_n + 1)
 
+      self.message.add('epoch', epoch)
       self.message.add('step', global_step)
       self.message.add('accuracy', accuracy, format='.5f')
       self.message.add('loss', loss, format='.5f')
@@ -205,6 +201,7 @@ class Evaluator:
     if self.best_accuracy is None or self.best_accuracy < accuracy:
       self.best_global_step = global_step
       self.best_accuracy = accuracy
+    self.message.add('--> epoch', epoch)
     self.message.add('step', global_step)
     self.message.add('accuracy', accuracy, format='.5f')
     self.message.add('loss', loss, format='.5f')
@@ -251,7 +248,7 @@ class Evaluator:
         }
         self.dump.files(results)
 
-      self.message.add('step', global_step)
+      self.message.add('', socket.gethostname())
       self.message.add('accuracy', accuracy, format='.5f')
       self.message.add('loss', loss, format='.5f')
       self.message.add('imgs/sec', examples_per_second, format='.0f')
@@ -259,7 +256,7 @@ class Evaluator:
 
     self.best_global_step = global_step
     self.best_accuracy = accuracy
-    self.message.add('step', global_step)
+    self.message.add('', socket.gethostname())
     self.message.add('accuracy', accuracy, format='.5f')
     self.message.add('loss', loss, format='.5f')
     logging.info(self.message.get_message())
