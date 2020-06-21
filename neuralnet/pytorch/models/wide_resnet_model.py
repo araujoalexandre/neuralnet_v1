f43f7d56e1185d51fef799ca5828a9ed3ce273af
Alexandre Araujo <aaraujo001@gmail.com>
2020-06-21 11:00:26 +0200

    update wide resnet build script

--- neuralnet/pytorch/models/wide_resnet_model.py
+++ neuralnet/pytorch/models/wide_resnet_model.py
@@ -26,12 +26,15 @@ def conv_init(m):
 
 
 class wide_basic(nn.Module):
-  def __init__(self, in_planes, planes, dropout_rate, stride=1, bias=True):
+  def __init__(self, in_planes, planes, dropout, stride=1, leaky_slope=0.0,
+               bias=True):
     super(wide_basic, self).__init__()
+    self.leaky_relu = nn.LeakyReLU(
+      negative_slope=leaky_slope, inplace=False)
     self.bn1 = nn.BatchNorm2d(in_planes)
     self.conv1 = nn.Conv2d(
       in_planes, planes, kernel_size=3, padding=1, bias=bias)
-    self.dropout = nn.Dropout(p=dropout_rate)
+    self.dropout = nn.Dropout(p=dropout)
     self.bn2 = nn.BatchNorm2d(planes)
     self.conv2 = nn.Conv2d(
       planes, planes, kernel_size=3, stride=stride, padding=1, bias=bias)
@@ -42,8 +45,8 @@ class wide_basic(nn.Module):
         nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=bias))
 
   def forward(self, x):
-    out = self.dropout(self.conv1(F.relu(self.bn1(x))))
-    out = self.conv2(F.relu(self.bn2(out)))
+    out = self.dropout(self.conv1(self.leaky_relu(self.bn1(x))))
+    out = self.conv2(self.leaky_relu(self.bn2(out)))
     out += self.shortcut(x)
 
     return out
@@ -63,21 +66,25 @@ class WideResnetModel(nn.Module):
 
     k = config['widen_factor']
     depth = config['depth']
-    dropout_rate = config['dropout']
-    bias = config.get('bias', None) or True
+    self.dropout = config['dropout']
+    self.leaky_slope = config['leaky_slope']
+    self.bias = config.get('bias', None) or True
 
     assert ((depth-4) % 6 == 0), 'Wide-resnet depth should be 6n+4'
     n = (depth-4)/6
 
     nStages = [16, 16*k, 32*k, 64*k]
 
+    self.leaky_relu = nn.LeakyReLU(
+      negative_slope=self.leaky_slope, inplace=False)
+
     self.conv1 = conv3x3(3, nStages[0])
     self.layer1 = self._wide_layer(
-      wide_basic, nStages[1], n, dropout_rate, stride=1, bias=bias)
+      wide_basic, nStages[1], n, stride=1)
     self.layer2 = self._wide_layer(
-      wide_basic, nStages[2], n, dropout_rate, stride=2, bias=bias)
+      wide_basic, nStages[2], n, stride=2)
     self.layer3 = self._wide_layer(
-      wide_basic, nStages[3], n, dropout_rate, stride=2, bias=bias)
+      wide_basic, nStages[3], n, stride=2)
     self.bn1 = nn.BatchNorm2d(nStages[3], momentum=0.9)
 
     if self.config.get('use_dc_last', False):
@@ -87,11 +94,13 @@ class WideResnetModel(nn.Module):
     else:
       self.linear = nn.Linear(nStages[3], num_classes)
 
-  def _wide_layer(self, block, planes, num_blocks, dropout_rate, stride, bias=False):
+  def _wide_layer(self, block, planes, num_blocks, stride):
     strides = [stride] + [1]*int(num_blocks-1)
     layers = []
     for stride in strides:
-      layers.append(block(self.in_planes, planes, dropout_rate, stride, bias=bias))
+      layers.append(block(
+        self.in_planes, planes, self.dropout, stride,
+        leaky_slope=self.leaky_slope, bias=self.bias))
       self.in_planes = planes
     return nn.Sequential(*layers)
 
@@ -100,7 +109,7 @@ class WideResnetModel(nn.Module):
     out = self.layer1(out)
     out = self.layer2(out)
     out = self.layer3(out)
-    out = F.relu(self.bn1(out))
+    out = self.leaky_relu(self.bn1(out))
     out = F.avg_pool2d(out, 8)
     out = out.view(out.size(0), -1)
     out = self.linear(out)
